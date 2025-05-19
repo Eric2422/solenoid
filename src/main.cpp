e5b42c991ca9f679df6f6e9085514217d88c8ddc
Eric Zhang <ericbkzhang@gmail.com>
2025-05-19 13:10:24 -0400

    Set up necessary constants

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "sci_constants.h"
+
+double calculateCylinderEffectivePermeability(double radius, double length, double relativePermeability) {
+    // An approximation for the effective permeability of a cylinder.
+    // Based on
+    double demagnetizingFactor = 1.0;
+}
+
+int main() {
+    std::cout << MU_0;
+    return EXIT_SUCCESS;
+}
---
+++ src/sci_constants.h
@@ -0,0 +1,5 @@
+#include <iostream>
+#include <numbers>
+
+const double PI = std::numbers::pi;
+const double MU_0 = 7.2973525643e-3;
