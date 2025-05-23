f48df646b775b80dcb726b88a89c455829055141
Eric Zhang <ericbkzhang@gmail.com>
2025-05-23 09:29:06 -0400

    Finsih wire length integration

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,70 @@
 
 #include "sci_constants.h"
 
-double calculateWireLength(double length, double radius, double turns, int )
-
-
-double calculateWireLength(double length, double radius, double turns) {
+/**
+ * @brief Calculate the length of wire in a solenoid
+ *
+ * Uses numerical integration to calculate the length of wire.
+ * 
+ * @param coilLength The length of the solenoid in meters(m)
+ * @param radius The radius of the solenoid in meters(m)
+ * @param numTurns How many turns of wire are in the solenoid
+ * @param numIntervals How many integration intervals to divide the coil into
+ * @return The length of wire in the solenoid in meters(m)
+ */
+double calculateWireLength(double coilLength, double radius, double numTurns, long numIntervals) {
     double length = 0;
+    double intervalLength = coilLength / numIntervals;
+    double omega = 2 * PI * numTurns / coilLength;
 
-    //
-    for (int i =0; i < turns * )
+    for (long i=0; i<numIntervals; i++) {
+        double x = i * intervalLength;
+
+        double dx = intervalLength;
+        double dy = radius * omega * -sin(omega * x) * intervalLength;
+        double dz = radius * omega * cos(omega * x) * intervalLength;
+
+        length += sqrt(pow(dx, 2) + pow(dy, 2) + pow(dz, 2));
+    }
+
+    return 2 * length;
 }
 
+/**
+ * @brief Calculate the length of wire in a solenoid
+ *
+ * Uses numerical integration to calculate the length of wire.
+ * Overloads @see double calculateWireLength(double coilLength, double radius, double numTurns, int numIntervals)
+ * to use a default value of 100 intervals per turn of wire. 
+ * 
+ * 
+ * @param coilLength The length of the solenoid in meters(m)
+ * @param radius The radius of the solenoid in meters(m)
+ * @param numTurns How many turns of wire are in the solenoid
+ * @return The length of wire in the solenoid in meters(m)
+ */
+double calculateWireLength(double coilLength, double radius, long numTurns) {
+    long numIntervals = numTurns * 100;
+
+    return calculateWireLength(coilLength, radius, numTurns, numIntervals);
+}
+
+/**
+ * @brief Calulate the resistance of a cylindrical solenoid
+ * 
+ * @param wireLength The length of the wire wrapped aroudn the solenoid in meters(m)
+ * @param wireResistivity The resistivity of the wire in ohm-meters(Ω * m)
+ * @param wireDiameter The diameter of the wire in meters(m)
+ * @return The resistance of the wire in ohms(Ω) 
+ */
+double calculateSolenoidResistance(double wireLength, double wireResistivity, double wireDiameter) {
+    // The cross-sectional area of the wire
+    double radius = wireDiameter / 2;
+    double area = PI * pow(radius, 2);
+
+    // The resistance of the wire
+    return wireLength * wireResistivity / area;
+}
 
 /**
  * @brief Calculate the approximate effective permeability of a cylinder.
@@ -41,38 +95,37 @@ double calculateCylinderEffectivePermeability(double radius, double length, doub
  * @brief Calculate the magnetic field produced by a solenoid
  *
  * @param effectivePermeability The effective permeability of the solenoid core in N * A^-2
- * @param turns How many turns of wire are in the solenoid
+ * @param numTurns How many turns of wire are in the solenoid
  * @param length The length of the solenoid in meters(m)
  * @param current The current flowing through the solenoid in Amperes(A)
  * @return The magnitude of the magnetic field produced by the solenoid in Teslas(T)
  */
-double calculateSolenoidMagneticField(double effectivePermeability, double turns, double length, double current)
+double calculateSolenoidMagneticField(double effectivePermeability, double numTurns, double length, double current)
 {
-    return MU_0 * effectivePermeability * turns / length * current;
+    return MU_0 * effectivePermeability * numTurns / length * current;
 }
 
 int main()
 {
-    double length;
-    std::cout << "Length of solenoid(m):";
-    std::cin >> length;
+    // double length;
+    // std::cout << "Length of solenoid(m):";
+    // std::cin >> length;
 
-    double radius;
-    std::cout << "Radius of solenoid(m):";
-    std::cin >> radius;
+    // double radius;
+    // std::cout << "Radius of solenoid(m):";
+    // std::cin >> radius;
 
-    double turns;
-    std::cout << "Number of turns:";
-    std::cin >> turns;
+    // double turns;
+    // std::cout << "Number of turns:";
+    // std::cin >> turns;
 
-    double wireResisitivity;
-    std::cout << "Wire resistivity (ohms / m):";
-    std::cin >> turns;
+    // double wireResisitivity;
+    // std::cout << "Wire resistivity (ohms / m):";
+    // std::cin >> wireResisitivity;
 
-    double wireDiameter;
-    std::cout << ":";
-    std::cin >> turns;
+    // double wireDiameter;
+    // std::cout << "Wire diameter:";
+    // std::cin >> wireDiameter;
 
-    std::cout << MU_0;
     return EXIT_SUCCESS;
 }
