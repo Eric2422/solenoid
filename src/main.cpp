ddf1fe65bba7bf6598ae3a7b509c0963924e40d6
Eric Zhang <ericbkzhang@gmail.com>
2025-05-29 20:45:41 -0400

    Hopefully finished

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 
 #include "json.hpp"
+using json = nlohmann::json;
 
 #include "sci_constants.h"
 
@@ -12,24 +13,26 @@
  * @brief Calculate the length of wire in a solenoid
  *
  * Uses numerical integration to calculate the length of wire.
- * 
+ *
  * @param coilLength The length of the solenoid in meters(m)
  * @param radius The radius of the solenoid in meters(m)
  * @param numTurns How many turns of wire are in the solenoid
  * @param numIntervals How many integration intervals to divide the coil into
  * @return The length of wire in the solenoid in meters(m)
  */
-double calculateWireLength(double coilLength, double radius, double numTurns, long numIntervals) {
+double calculateWireLength(double coilLength, double coilRadius, double numTurns, long numIntervals)
+{
     double length = 0;
     double intervalLength = coilLength / numIntervals;
     double omega = 2 * PI * numTurns / coilLength;
 
-    for (long i=0; i<numIntervals; i++) {
+    for (long i = 0; i < numIntervals; i++)
+    {
         double x = i * intervalLength;
 
         double dx = intervalLength;
-        double dy = radius * omega * -sin(omega * x) * intervalLength;
-        double dz = radius * omega * cos(omega * x) * intervalLength;
+        double dy = coilRadius * omega * -sin(omega * x) * intervalLength;
+        double dz = coilRadius * omega * cos(omega * x) * intervalLength;
 
         length += sqrt(pow(dx, 2) + pow(dy, 2) + pow(dz, 2));
     }
@@ -42,32 +45,34 @@ double calculateWireLength(double coilLength, double radius, double numTurns, lo
  *
  * Uses numerical integration to calculate the length of wire.
  * Overloads @see double calculateWireLength(double coilLength, double radius, double numTurns, int numIntervals)
- * to use a default value of 100 intervals per turn of wire. 
- * 
- * 
+ * to use a default value of 100 intervals per turn of wire.
+ *
+ *
  * @param coilLength The length of the solenoid in meters(m)
  * @param radius The radius of the solenoid in meters(m)
  * @param numTurns How many turns of wire are in the solenoid
  * @return The length of wire in the solenoid in meters(m)
  */
-double calculateWireLength(double coilLength, double radius, long numTurns) {
+double calculateWireLength(double coilLength, double coilRadius, long numTurns)
+{
     long numIntervals = numTurns * 100;
 
-    return calculateWireLength(coilLength, radius, numTurns, numIntervals);
+    return calculateWireLength(coilLength, coilRadius, numTurns, numIntervals);
 }
 
 /**
  * @brief Calulate the resistance of a cylindrical solenoid
- * 
+ *
  * @param wireLength The length of the wire wrapped aroudn the solenoid in meters(m)
- * @param wireResistivity The resistivity of the wire in ohm-meters(Ω * m)
  * @param wireDiameter The diameter of the wire in meters(m)
- * @return The resistance of the wire in ohms(Ω) 
+ * @param wireResistivity The resistivity of the wire in ohm-meters(Ω * m)
+ * @return The resistance of the wire in ohms(Ω)
  */
-double calculateSolenoidResistance(double wireLength, double wireResistivity, double wireDiameter) {
+double calculateWireResistance(double wireLength, double wireDiameter, double wireResistivity)
+{
     // The cross-sectional area of the wire
-    double radius = wireDiameter / 2;
-    double area = PI * pow(radius, 2);
+    double coilRadius = wireDiameter / 2;
+    double area = PI * pow(coilRadius, 2);
 
     // The resistance of the wire
     return wireLength * wireResistivity / area;
@@ -85,10 +90,10 @@ double calculateSolenoidResistance(double wireLength, double wireResistivity, do
  * @param relativePermeability The relative permeability of the cylinder's material in N * A^-2
  * @return The effective permeability of the cylinder in N * A^-2
  */
-double calculateCylinderEffectivePermeability(double radius, double length, double relativePermeability)
+double calculateCylinderEffectivePermeability(double coilRadius, double coilLength, double relativePermeability)
 {
     // The ratio of the cylinder's length to its diameter
-    double n = length / (2 * radius);
+    double n = coilLength / (2 * coilRadius);
     double demagnetizingFactor = 1.0 / (2 * (2 * n / sqrt(PI)) + 1.0);
     double effectivePermeability = relativePermeability / (1.0 + demagnetizingFactor * (relativePermeability - 1.0));
 
@@ -115,9 +120,26 @@ int main()
     json data = json::parse(file);
     file.close();
 
-    double coilLength = data["coilLength"]
+    std::string fields[] = {"coilLength", "coilRadius", "numTurns", "relativePermeability", "wireDiameter", "wireResistivity", "voltage"};
+    for (const auto &field : fields)
+    {
+        if (!data.contains(field))
+        {
+            std::cerr << "The JSON is missing the \"" << field << "\" field." << std::endl;
 
-    std::cout << coilLength;
+            return EXIT_FAILURE;
+        }
+    }
+
+    double effectivePermeability = calculateCylinderEffectivePermeability(data["coilRadius"], data["coilLength"], data["relativePermeability"]);
+
+    double wireLength = calculateWireLength(data["coilLength"], data["coilRadius"], data["numTurns"]);
+    double wireResistance = calculateWireResistance(wireLength, data["wireDiameter"], data["wireResistivity"]);
+    double current = data["voltage"].get<double>() / wireResistance;
+
+    double magneticField = calculateSolenoidMagneticField(effectivePermeability, data["numTurns"], data["coilLength"], current);
+
+    std::cout << "B = " << magneticField << " T * A^-2" << std::endl;
 
     return EXIT_SUCCESS;
 }
